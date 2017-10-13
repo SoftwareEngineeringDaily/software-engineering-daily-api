873488d3c27faf34fd24aebf27f153f1e651ca3f
jason <bjason@gmail.com>
2017-10-13 15:25:20 -0700

    Remove extra blank lines.

--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -22,10 +22,8 @@ router.route('/:postId')
 router.route('/:postId/comments')
   .get(expressJwt({ secret: config.jwtSecret, credentialsRequired: false }), commentCtrl.list);
 
-
 router.route('/:postId/comment')
   .post(expressJwt({ secret: config.jwtSecret }), commentCtrl.create);
-
 
 router.route('/:postId/upvote')
   .post(expressJwt({ secret: config.jwtSecret }), voteCtrl.upvote);
