f3323b0a27b51fc73d32c2d7faa76782690c6807
jason <bjason@gmail.com>
2017-10-23 16:16:45 -0700

    Removing unused routes + updating validation

--- config/param-validation.js
+++ config/param-validation.js
@@ -12,8 +12,8 @@ export default {
   // UPDATE /api/users/:userId
   updateUser: {
     body: {
-      username: Joi.string().required(),
-      mobileNumber: Joi.string().regex(/^[1-9][0-9]{9}$/).required()
+      username: Joi.string().required()
+      // , mobileNumber: Joi.string().regex(/^[1-9][0-9]{9}$/).required()
     },
     params: {
       userId: Joi.string().hex().required()
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -5,12 +5,6 @@ import userCtrl from '../controllers/user.controller';
 
 const router = express.Router(); // eslint-disable-line new-cap
 
-router.route('/')
-  /** GET /api/users - Get list of users */
-  .get(userCtrl.list)
-
-  /** POST /api/users - Create new user */
-  .post(validate(paramValidation.createUser), userCtrl.create);
 
 router.route('/:userId')
   /** GET /api/users/:userId - Get user */
@@ -19,8 +13,6 @@ router.route('/:userId')
   /** PUT /api/users/:userId - Update user */
   .put(validate(paramValidation.updateUser), userCtrl.update)
 
-  /** DELETE /api/users/:userId - Delete user */
-  .delete(userCtrl.remove);
 
 /** Load user when API with userId route parameter is hit */
 router.param('userId', userCtrl.load);
