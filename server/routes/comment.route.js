b62fdf29dee8f9da2c425f5cd84ac7b68ab6f17d
jason <bjason@gmail.com>
2017-11-07 10:16:27 -0800

    Basic comment route.

--- server/routes/comment.route.js
+++ server/routes/comment.route.js
@@ -1,23 +1,26 @@
-// TODO: need to do comment vote routes here otherwise post will be
-// preloaded
 import express from 'express';
 import expressJwt from 'express-jwt';
+import transferField from '../middleware/transferField';
+import commentCtrl from '../controllers/comment.controller';
 import voteCtrl from '../controllers/vote.controller';
 import config from '../../config/config';
+import commentCtrl from '../controllers/comment.controller';
 
 const router = express.Router(); // eslint-disable-line new-cap
 
 
 router.route('/:commentId/upvote')
   .post(expressJwt({ secret: config.jwtSecret })
-  // , voteCtrl.movePostToEntity TODO: moveToEntity, higher order function
   // middleware:
-  // transferField({source: 'comment', target: 'entity'})
   // TODO: refactor to have these into one call like upvote: [method1, method2,...]
   // upvoteHandlers
+  , transferField({source: 'comment', target: 'entity'})
   , voteCtrl.findVote
   , voteCtrl.upvote // rename to upvoteHelper
   , voteCtrl.finish // IF we add a model.unlike we don't really need this..
 );
 
 // TODO: load comment
+router.param('commentId', commentCtrl.load);
+
+export default router;
