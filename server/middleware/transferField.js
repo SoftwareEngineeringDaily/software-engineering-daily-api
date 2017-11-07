34f9c304c161757f3f7e0d063bdf02083f2b358f
jason <bjason@gmail.com>
2017-11-07 10:07:45 -0800

    Simplify return function

--- server/middleware/transferField.js
+++ server/middleware/transferField.js
@@ -1,12 +1,11 @@
 // This copies a req.someSource to req.someTarget;
 function transferField({ source, target }) {
-  function cb(req, res, next)  {
+  return (req, res, next) => {
     if ( req[source] ) {
       req[target] = req[source];
     }
     next();
   }
-  return cb;
 }
 
 
