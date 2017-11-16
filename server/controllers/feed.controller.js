fc13bf8359e094838e6aa4c38f3dbc09d2409d77
jason <bjason@gmail.com>
2017-11-16 13:16:36 -0800

    Add feed routes + models.

--- server/controllers/feed.controller.js
+++ server/controllers/feed.controller.js
@@ -1,4 +1,18 @@
 import Promise from 'bluebird';
 import mongoose from 'mongoose';
-
+import httpStatus from 'http-status';
+import APIError from '../helpers/APIError';
 import Feed from '../models/Feed.model';
+
+function list(req, res, next) {
+  Feed.findOne({user: req.user._id})
+  .then(({feedItems}) => {
+    res.json(feedItems);
+  })
+  .catch((error) => {
+      const err = new APIError('Error fetching user feed', httpStatus.INTERNAL_SERVER_ERROR, true); //eslint-disable-line
+      return next(err);
+  });
+};
+
+export default {list};
