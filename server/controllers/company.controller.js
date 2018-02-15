7a445c5b3b7bc6456e725bcb091e27c5a46d8039
jason <bjason@gmail.com>
2018-02-14 17:12:07 -0800

    ensureIsAdmin middleware

--- server/controllers/company.controller.js
+++ server/controllers/company.controller.js
@@ -5,13 +5,13 @@ import APIError from '../helpers/APIError';
 export default {
 
   list: async (req, res, next) => {
-  }
+  },
 
   create: async (req, res, next) => {
     try {
       const newCompany = new Company(req.body);
       await newCompany.save();
-      return res.status(httpStatus.CREATED).json(newCompany.toObject(), true));
+      return res.status(httpStatus.CREATED).json(newCompany.toObject(), true);
     } catch (err) {
       return next(err);
     }
