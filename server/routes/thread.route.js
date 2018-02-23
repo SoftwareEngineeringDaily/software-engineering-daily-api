e0f951270b14d62dfa828faab5d2d4a525c45c20
Thanh Vu <v2tmobile.it@gmail.com>
2018-02-23 23:00:23 +0700

    implement forum thread API

---
+++ server/routes/thread.route.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import expressJwt from 'express-jwt';
+import config from '../../config/config';
+import threadController from '../controllers/thread.controller';
+
+const router = express.Router();
+
+router
+  .route('/')
+  .get(expressJwt({ secret: config.jwtSecret, credentialsRequired: false }), threadController.list)
+  .post(
+    expressJwt({ secret: config.jwtSecret, credentialsRequired: true }),
+    threadController.create
+  );
+
+router
+  .route('/:threadId')
+  .get(
+    expressJwt({ secret: config.jwtSecret, credentialsRequired: false }),
+    threadController.detail
+  )
+  .put(expressJwt({ secret: config.jwtSecret, credentialsRequired: true }), threadController.update)
+  .delete(
+    expressJwt({ secret: config.jwtSecret, credentialsRequired: true }),
+    threadController.delete
+  );
+
+export default router;
