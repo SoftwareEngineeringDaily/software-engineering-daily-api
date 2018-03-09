7432499936f040014d7b1a449542baf3aa4f1200
jason <bjason@gmail.com>
2018-03-08 16:55:44 -0800

    List and create forum threads

--- server/controllers/forum.controller.js
+++ server/controllers/forum.controller.js
@@ -9,13 +9,36 @@ function load(req, res, next, id) {
     .catch(e => next(e));
 }
 
+function list(req, res, next) {
+  const {
+    limit = null,
+    createdAtBefore = null,
+    createdAfter = null,
+    search = null
+  } = req.query;
 
-function list() {
+  const query = {};
+  if (limit) query.limit = Math.min(limit, 500);
+  if (createdAtBefore) query.createdAtBefore = createdAtBefore;
+  if (createdAfter) query.createdAfter = createdAfter;
+  if (search) query.search = search;
+
+  ForumThread.list(query)
+    .then(posts => res.json(posts))
+    .catch(e => next(e));
 }
+
 function detail() {
 }
 
-function create() {
+function create(req, res, next) {
+  const forumThread = new ForumThread();
+  forumThread
+    .save()
+    .then((forumThreadSaved) => {
+      res.status(201).json(forumThreadSaved);
+    })
+    .catch(err => next(err));
 }
 
 function update() {
