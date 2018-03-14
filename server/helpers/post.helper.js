74b3e649e3aa71b48388817bc1980dc76c12f2e7
jason <bjason@gmail.com>
2018-03-14 15:14:39 -0700

    Simple debug mp3

--- server/helpers/post.helper.js
+++ server/helpers/post.helper.js
@@ -1,6 +1,10 @@
 import config from '../../config/config';
 
 function replaceWithAdFree(post) {
+  console.log('--mp3', post.mp3); // eslint-disable-line
+  if (post.toObject) {
+    console.log('--mp3 TO OBJECT', post.toObject().mp3); // eslint-disable-line
+  }
   try {
     const originalMP3Split = post.mp3.split('/');
     if (originalMP3Split.length > 0) {
@@ -23,12 +27,12 @@ function getAdFreeSinglePostIfSubscribed(post, fullUser, next) {
 }
 
 function getAdFreePostsIfSubscribed(posts, fullUser, next) {
-  if (fullUser && fullUser.subscription && fullUser.subscription.active) {
-    // Here we do this so we can fetch subscritions:
-    const _posts = posts.map(post => replaceWithAdFree(post, next));
-    return _posts;
-  }
-  return posts;
+  // if (fullUser && fullUser.subscription && fullUser.subscription.active) {
+  // Here we do this so we can fetch subscritions:
+  const _posts = posts.map(post => replaceWithAdFree(post, next));
+  return _posts;
+  // }
+  // return posts;
 }
 
 export { replaceWithAdFree, getAdFreeSinglePostIfSubscribed, getAdFreePostsIfSubscribed };
