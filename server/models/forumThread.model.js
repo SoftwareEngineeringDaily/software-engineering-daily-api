d0c6ed0c7bee4b52b7c58c85584027b117e9c254
jason <bjason@gmail.com>
2018-03-19 16:03:15 -0700

    Comments count refactor

--- server/models/forumThread.model.js
+++ server/models/forumThread.model.js
@@ -1,9 +1,6 @@
 import mongoose from 'mongoose';
-import map from 'lodash/map';
-import each from 'lodash/each';
 import httpStatus from 'http-status';
 import APIError from '../helpers/APIError';
-import Comment from './comment.model';
 
 const ForumThreadSchema = new mongoose.Schema({
   id: String,
@@ -52,7 +49,7 @@ ForumThreadSchema.statics = {
   increaseCommentCount(id) {
     return this.get(id).then((thread) => {
       const forumThread = thread;
-      forumThread.commentCount += 1;
+      forumThread.commentsCount += 1;
       forumThread.dateLastAcitiy = new Date();
       return forumThread.save();
     });
@@ -64,44 +61,7 @@ ForumThreadSchema.statics = {
     return this.find(query)
       .populate('author', '-password')
       .sort({ dateLastAcitiy: -1 })
-      .exec()
-      .then((threads) => {
-        // Let's fill in the commentCount for each thread.
-        // First we get the ids in an array:
-        const threadIds = map(threads, (thread) => {
-          // Get Thread ids
-          console.log('thread');
-          return thread._id;
-        });
-        return Comment.aggregate([
-          // Restrict to subset of threads (todo: paginate).
-          { $match: { rootEntity: { $in: threadIds } } },
-          { $group: { _id: '$rootEntity', count: { $sum: 1 } } }
-        ])
-          .then((counts) => {
-            // TODO: loop and make commentCount = zero for all threads.
-            // TODO: loop through threads and add counts:
-            const expandedThreads = map(threads, (thread) => {
-              const expandedThread = Object.assign({}, thread.toObject(), { commentCount: 0 });
-              return expandedThread;
-            });
-            // Let's make a map of our counts to make it easier to look up:
-            const countsMap = {};
-            each(counts, (count) => {
-              countsMap[count._id] = count.count;
-            });
-
-            /* eslint-disable no-param-reassign */
-            each(expandedThreads, (expandedThread) => {
-              if (countsMap[expandedThread._id]) {
-                expandedThread.commentCount = countsMap[expandedThread._id];
-              }
-            });
-            /* eslint-enable no-param-reassign */
-
-            return expandedThreads;
-          });
-      });
+      .exec();
   }
 
   /*
