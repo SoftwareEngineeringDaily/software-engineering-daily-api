505af9c8ce023324c33db7f0ff2afdbf8d4cf774
jason <bjason@gmail.com>
2018-03-20 15:07:11 -0700

    Vote transformation for generic entity

--- server/models/forumThread.model.js
+++ server/models/forumThread.model.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import httpStatus from 'http-status';
 import APIError from '../helpers/APIError';
+import Vote from './vote.model';
 
 const ForumThreadSchema = new mongoose.Schema({
   id: String,
@@ -62,7 +63,50 @@ ForumThreadSchema.statics = {
       .populate('author', '-password')
       .sort({ dateLastAcitiy: -1 })
       .exec();
+  },
+  addVotesForUserToPosts(entities, userId) {
+    const ids = posts.map((entity) => { //eslint-disable-line
+      return entity._id;
+    });
+    return Vote.find({
+      userId,
+      entityId: { $in: ids },
+    })
+      .exec()
+      .then((votes) => {
+        const voteMap = {};
+        for (let index in votes) { // eslint-disable-line
+          const vote = votes[index];
+          const voteKey = vote.entityId;
+          voteMap[voteKey] = vote;
+        }
+
+        const updatedEntities = [];
+        for (let index in entities) { // eslint-disable-line
+          const entity = entities[index];
+          entity.upvoted = false;
+          entity.downvoted = false;
+
+          if (!voteMap[entity._id]) {
+            updatedEntities.push(entity);
+            continue; // eslint-disable-line
+          }
+
+          if (voteMap[entity._id].direction === 'upvote' && voteMap[entity._id].active) {
+            entity.upvoted = true;
+          }
+
+          if (voteMap[entity._id].direction === 'downvote' && voteMap[entity._id].active) {
+            entity.downvoted = true;
+          }
+
+          updatedEntities.push(entity);
+        }
+
+        return updatedEntities;
+      });
   }
+
 
   /*
   list() {
