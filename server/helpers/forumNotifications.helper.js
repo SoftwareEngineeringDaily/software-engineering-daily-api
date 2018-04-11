96ce9ba5d6abff59730be933654a906b473a68b4
jason <bjason@gmail.com>
2018-04-11 10:30:44 -0700

    Notifications only if user is allowing them.

--- server/helpers/forumNotifications.helper.js
+++ server/helpers/forumNotifications.helper.js
@@ -18,22 +18,23 @@ async function sendForumNotificationEmail({ threadId, userWhoReplied }) {
   try {
     const userIdWhoReplied = userWhoReplied._id;
     const userDesc = getUserDescription(userWhoReplied);
-    ForumThread.get(threadId)
-      .then((thread) => {
-        const { email, _id } = thread.author;
-        // Don't email if you are the author and replying to own stuff:
-        if (userIdWhoReplied.toString() === _id.toString()) return;
+    const thread = await ForumThread.get(threadId);
+    const userToEmail = thread.author;
+    const { email, _id } = thread.author;
+    if (userToEmail.emailNotiicationSettings &&
+      userToEmail.emailNotiicationSettings.unsubscribedFromThreads) return;
 
-        const msg = {
-          to: email,
-          from: 'no-reply@softwaredaily.com',
-          subject: 'Someone commented on your thread @SoftwareDaily',
-          text: `${userDesc} commented in your thread: ${config.baseUrl}/forum/${threadId}`,
-          html: `${userDesc} replied to your thread, view thread: <strong> <a href="${config.baseUrl}/forum/${threadId}/"> here </a>.
-          <br /><br /> <a href="${config.baseUrl}/notification-settings/"> Unsubscribe </a>`
-        };
-        sgMail.send(msg);
-      });
+    // Don't email if you are the author and replying to own stuff:
+    if (userIdWhoReplied.toString() === _id.toString()) return;
+    const msg = {
+      to: email,
+      from: 'no-reply@softwaredaily.com',
+      subject: 'Someone commented on your thread @SoftwareDaily',
+      text: `${userDesc} commented in your thread: ${config.baseUrl}/forum/${threadId}`,
+      html: `${userDesc} replied to your thread, view thread: <strong> <a href="${config.baseUrl}/forum/${threadId}/"> here </a>.
+      <br /><br /> <a href="${config.baseUrl}/notification-settings/"> Unsubscribe </a>`
+    };
+    sgMail.send(msg);
   } catch (e) {
     console.log('Error emailing notification', e);
   }
@@ -47,22 +48,23 @@ async function sendReplyEmailNotificationEmail({ parentCommentId, threadId, user
   try {
     const userIdWhoReplied = userWhoReplied._id;
     const userDesc = getUserDescription(userWhoReplied);
-    Comment.get(parentCommentId)
-      .then((parentComment) => {
-        const { email, _id } = parentComment.author;
-        // Don't email if you are the author and replying to own stuff:
-        if (userIdWhoReplied.toString() === _id.toString()) return;
+    const parentComment = await Comment.get(parentCommentId);
+    const userToEmail = parentComment.author;
+    const { email, _id } = parentComment.author;
+    if (userToEmail.emailNotiicationSettings &&
+      userToEmail.emailNotiicationSettings.unsubscribedFromCommentReplies) return;
+    // Don't email if you are the author and replying to own stuff:
+    if (userIdWhoReplied.toString() === _id.toString()) return;
 
-        const msg = {
-          to: email,
-          from: 'no-reply@softwaredaily.com',
-          subject: 'Someone replied to you in the SoftwareDaily Forum',
-          text: `${userDesc} replied to your comment: ${config.baseUrl}/forum/${threadId}`,
-          html: `${userDesc} replied to your comment. View it: <strong> <a href="${config.baseUrl}/forum/${threadId}/"> here </a>.
-          <br /><br /> <a href="${config.baseUrl}/notification-settings/"> Unsubscribe </a>`
-        };
-        sgMail.send(msg);
-      });
+    const msg = {
+      to: email,
+      from: 'no-reply@softwaredaily.com',
+      subject: 'Someone replied to you in the SoftwareDaily Forum',
+      text: `${userDesc} replied to your comment: ${config.baseUrl}/forum/${threadId}`,
+      html: `${userDesc} replied to your comment. View it: <strong> <a href="${config.baseUrl}/forum/${threadId}/"> here </a>.
+      <br /><br /> <a href="${config.baseUrl}/notification-settings/"> Unsubscribe </a>`
+    };
+    sgMail.send(msg);
   } catch (e) {
     console.log('Error emailing notification:', e);
   }
