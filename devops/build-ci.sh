297fbb95c5b88f397c98e3039f0ea0d4b5dca5d1
Andrew Lloyd <andrew85.lloyd@gmail.com>
2018-05-06 20:43:51 -0700

    Only build docker image if a CI build

--- devops/build-ci.sh
+++ devops/build-ci.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 # builds sedaiy-rest-api image for Continuous Integration (CI) purposes
 
-echo $CI_BUILD
+# If not a CI build, don't build the Docker image
+if [ -z "$CI_BUILD" ]; then
+	exit 0
+fi
 
-#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-#REPO_DIR=$(dirname $DIR)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+REPO_DIR=$(dirname $DIR)
 
 # copy docker file to repo root
-#cp $DIR/ci.Dockerfile $REPO_DIR/ci.Dockerfile
+cp $DIR/ci.Dockerfile $REPO_DIR/ci.Dockerfile
 
-#docker build -f $REPO_DIR/ci.Dockerfile -t softwaredaily/sedaily-rest-api $REPO_DIR
+docker build -f $REPO_DIR/ci.Dockerfile -t softwaredaily/sedaily-rest-api $REPO_DIR
 
-#rm $REPO_DIR/ci.Dockerfile
+rm $REPO_DIR/ci.Dockerfile
 
 # docker login #must be part of organization
-#docker push softwaredaily/sedaily-rest-api
+docker push softwaredaily/sedaily-rest-api
