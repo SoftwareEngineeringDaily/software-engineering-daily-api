901df6089d6967b300b5c0a01a7b614ef6ad02da
Andrew Lloyd <andrew85.lloyd@gmail.com>
2018-05-06 20:50:17 -0700

    Use function and env var

--- devops/build-ci.sh
+++ devops/build-ci.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 # builds sedaiy-rest-api image for Continuous Integration (CI) purposes
 
+function buildApiImage {
+	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+	REPO_DIR=$(dirname $DIR)
+
+	# copy docker file to repo root
+	cp $DIR/ci.Dockerfile $REPO_DIR/ci.Dockerfile
+
+	docker build -f $REPO_DIR/ci.Dockerfile -t softwaredaily/sedaily-rest-api $REPO_DIR
+
+	rm $REPO_DIR/ci.Dockerfile
+
+	# docker login #must be part of organization
+	docker push softwaredaily/sedaily-rest-api	
+}
+
 # If not a CI build, don't build the Docker image
-if [ -z "$CI_BUILD" ]; then
-	exit 0
+if [ "$CI_BUILD" = 'true' ] ; then
+	echo 'Be careful not to fall off!'
 fi
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-REPO_DIR=$(dirname $DIR)
-
-# copy docker file to repo root
-cp $DIR/ci.Dockerfile $REPO_DIR/ci.Dockerfile
-
-docker build -f $REPO_DIR/ci.Dockerfile -t softwaredaily/sedaily-rest-api $REPO_DIR
-
-rm $REPO_DIR/ci.Dockerfile
-
-# docker login #must be part of organization
-docker push softwaredaily/sedaily-rest-api
