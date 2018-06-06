086abc9cee79fa05a95a775830c612daabd9ee8f
Andrew Lloyd <andrew85.lloyd@gmail.com>
2018-06-06 15:53:00 -0700

    Use CI specific environment variables

--- devops/build-mongodb.sh
+++ devops/build-mongodb.sh
@@ -8,11 +8,11 @@ fi
 
 DOCKER_IMAGE="softwaredaily/sedaily-mongo:$TAG"
 
-mongodump --host $MONGO_HOST --username $MONGO_USER --password $MONGO_PASS --port $MONGO_PORT -d $MONGO_DB --out ./backup
+mongodump --host $MONGO_CI_HOST --username $MONGO_CI_USER --password $MONGO_CI_PASS --port $MONGO_CI_PORT -d $MONGO_CI_DB --out ./backup
 
 mkdir dump/
-mv backup/$MONGO_DB/* dump/
-rm -rf $MONGO_DB
+mv backup/$MONGO_CI_DB/* dump/
+rm -rf $MONGO_CI_DB
 
 docker build -f mongo.Dockerfile -t $DOCKER_IMAGE . --no-cache
 
