c7b45430b62b5b80418af5c01053fcfc97011ed8
Andrew Lloyd <andrew85.lloyd@gmail.com>
2018-06-06 14:32:41 -0700

    Add ls in container

--- devops/build-mongodb.sh
+++ devops/build-mongodb.sh
@@ -2,7 +2,11 @@
 
 DOCKER_IMAGE="softwaredaily/sedaily-mongo:develop"
 
-docker run --rm -v "`pwd`/backup:/opt/backup" mongo:3.4.10 bash -c "mongodump --host $MONGO_HOST --username $MONGO_USER --password $MONGO_PASS --port $MONGO_PORT -d $MONGO_DB --out /opt/backup; chown -R 1000 /opt/backup"
+docker run --rm -v "`pwd`/backup:/opt/backup" mongo:3.4.10 bash -c "mongodump --host $MONGO_HOST --username $MONGO_USER --password $MONGO_PASS --port $MONGO_PORT -d $MONGO_DB --out /opt/backup; ls -l /opt/backup"
+
+ls -l
+ls -l backup/
+
 mkdir dump/
 mv backup/$MONGO_DB/* dump/
 rm -rf $MONGO_DB
