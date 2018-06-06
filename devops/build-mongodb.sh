bb786b42903d7686daa8acd5db1d6db898415df0
Andrew Lloyd <andrew85.lloyd@gmail.com>
2018-06-06 14:16:44 -0700

    run ls to see ownership

--- devops/build-mongodb.sh
+++ devops/build-mongodb.sh
@@ -3,7 +3,8 @@
 DOCKER_IMAGE="softwaredaily/sedaily-mongo:develop"
 
 docker run --rm -v "`pwd`/backup:/opt/backup" mongo:3.4.10 bash -c "mongodump --host $MONGO_HOST --username $MONGO_USER --password $MONGO_PASS --port $MONGO_PORT -d $MONGO_DB --out /opt/backup"
-
+ls -l
+ls -l backup
 mkdir dump/
 mv backup/$MONGO_DB/* dump/
 rm -rf $MONGO_DB
