f340a2bb968f3ca5b01fe1d9a6dbf9ad0f98f343
Andrew Lloyd <andrew85.lloyd@gmail.com>
2018-06-06 14:08:03 -0700

    Test building mongo image

--- devops/build-mongodb.sh
+++ devops/build-mongodb.sh
@@ -2,14 +2,19 @@
 
 . test.sh
 
+DOCKER_IMAGE="softwaredaily/sedaily-mongo:develop"
+
 docker run --rm -v "`pwd`/backup:/opt/backup" mongo:3.4.10 bash -c "mongodump --host $MONGO_HOST --username $MONGO_USER --password $MONGO_PASS --port $MONGO_PORT -d $MONGO_DB --out /opt/backup"
 
 mkdir dump/
 mv backup/$MONGO_DB/* dump/
 rm -rf $MONGO_DB
 
-docker build -f mongo.Dockerfile -t softwaredaily/sedaily-mongo . --no-cache
+docker build -f mongo.Dockerfile -t $DOCKER_IMAGE . --no-cache
 
 rm -rf dump
 
-#docker push softwaredaily/sedaily-mongo
+# must be part the organization
+echo $DOCKER_PASSWORD | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+docker push $DOCKER_IMAGE
