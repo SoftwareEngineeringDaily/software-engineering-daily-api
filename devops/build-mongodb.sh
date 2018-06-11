e59d79dd7ae992f729dc884b90cc4041b9e14db0
Andrew Lloyd <andrew85.lloyd@gmail.com>
2018-06-11 13:46:15 -0700

    Add Travis CI automation to create dockerized backend (#257)
    * Automate building Docker images of the API and MongoDB

---
+++ devops/build-mongodb.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ -z $1 ]]; then
+	TAG="develop"
+else
+	TAG=$1
+fi
+
+DOCKER_IMAGE="softwaredaily/sedaily-mongo:$TAG"
+
+mongodump --host $MONGO_CI_HOST --port $MONGO_CI_PORT --username $MONGO_CI_USER --password $MONGO_CI_PASS -d $MONGO_CI_DB --excludeCollection users --out ./backup
+
+mongodump --host $MONGO_CI_HOST --username $MONGO_CI_USER --password $MONGO_CI_PASS --port $MONGO_CI_PORT -d $MONGO_CI_DB --collection users --query '{ email: { $eq: "forum_admin@softwaredaily.com" } }' --out ./backup
+
+mkdir dump/
+mv backup/$MONGO_CI_DB/* dump/
+rm -rf $MONGO_CI_DB
+
+docker build -f mongo.Dockerfile -t $DOCKER_IMAGE . --no-cache
+
+rm -rf dump
+
+# must be part the organization
+echo $DOCKER_PASSWORD | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+docker push $DOCKER_IMAGE
