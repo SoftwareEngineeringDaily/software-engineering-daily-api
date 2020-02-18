b85c23af2e5d89efa9bb006a8072bb78c021ede8
Marcos Schlup <marcos.schlup@gmail.com>
2020-02-18 14:03:12 -0300

    Created postSubscription schema

---
+++ server/models/postSubscription.mode.js
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from 'mongoose';
+import config from '../../config/config';
+
+/**
+ * Schema
+ */
+const postSubscriptionSchema = new mongoose.Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  post: {
+    type: Schema.Types.ObjectId,
+    ref: 'Post'
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export default mongoose.model(`${config.mongo.collectionPrefix}Subscription`, postSubscriptionSchema);
