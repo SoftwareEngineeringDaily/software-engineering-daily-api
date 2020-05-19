5d3ccfd036b78cea2b4ecc1025371ee31f4906b5
Marcos Schlup <marcos.schlup@gmail.com>
2020-05-19 18:15:22 -0300

    Added fix to AdfreeMp3 to accept new secure route

--- server/helpers/mp3.helper.js
+++ server/helpers/mp3.helper.js
@@ -3,7 +3,7 @@ const privateMp3URL = 'https://s3-us-west-2.amazonaws.com/sd-profile-pictures/ad
 function getAdFreeMp3(originalMp3) {
   if (typeof originalMp3 !== 'string') return originalMp3;
 
-  const extractedFile = originalMp3.toString().match(/\/traffic.libsyn.com\/sedaily\/(.*?).mp3/);
+  const extractedFile = originalMp3.toString().match(/\/traffic.libsyn.co.+\/sedaily\/(.*?).mp3/);
 
   let privateMp3 = originalMp3;
 
